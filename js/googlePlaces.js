6987d3988ac633d37cdc7936634e897e05815543
canlas1 <dc@uscr.com>
2017-04-11 10:57:11 -0400

    Adjusted for special characters
    adjusted dennis’ branch for special characters

--- js/googlePlaces.js
+++ js/googlePlaces.js
@@ -12,6 +12,9 @@ $("#submitForm").on("click", function(event) {
     event.preventDefault();
     console.log(event);
     var term = $("#dba-input").val().trim();
+    // term = term.replace(/'/g, "%27");
+    // term = term.replace(/ /g, "%20");
+    // term = term.replace(/,/g, "%82");
     console.log("term: ", term);
     var area = $('#area-input').val().trim()
     console.log("area: ", area);
@@ -67,27 +70,31 @@ function callback(results, status) {
             console.log("zip split ", zipParts);
 
             var restName = placeName;
+
             console.log("restName for gov: ", restName);
             var building = streetParts[0];
             console.log("building for gov: ", building);
             var zipcode = zipParts[2];
             console.log("zipcode for gov: ", zipcode);
-
-
+                
+                // Modified restName for query
+                restName = restName.replace(/'/g, "%27");
+                restName = restName.replace(/ /g, "%20");
+                restName = restName.replace(/,/g, "%82");
 
 
             // var dbaSearch = placeName +
 
             var image = "<img src='" + placePhotoRef + "'>";
 
-            var buttonSelect = "<button class='selectBtn' " + "data-restName='" + restName + "' " + "data-building='" + building + "' " + "data-zipcode='" + zipcode + "'>Select</button>";
+            var buttonSelect = "<button class='selectBtn' " + "data-restName=\"" + restName + "\" data-building='" + building + "' " + "data-zipcode='" + zipcode + "'>Select</button>";
 
 
             console.log(placeId);
             console.log(placeName);
             console.log(placeAddress);
 
-             
+
 
             $(".table tbody").append("<tr><td class='restaurant-name-display'>" + placeName +
                 "</td><td class='address-display'>" + placeAddress +
@@ -115,93 +122,99 @@ function createMarker(place) {
     });
 }
 
-$("body").on("click",".selectBtn",function(){
-
+$("body").on("click", ".selectBtn", function() {
+console.log(this);
     $(".newsearch").css("display", "block");
     $(".searchres").css("display", "block");
 
     $(".table").css("display", "none");
     $("#map").css("display", "none");
 
-  var restName = $(this).data("restname");
-  console.log("del click event " + restName);
+    var restName = $(this).data("restname");
+    console.log("del click event " + restName);
 
-  var building = $(this).data("building");
-  console.log("del click event " + building);
+    var building = $(this).data("building");
+    console.log("del click event " + building);
 
-  var zipcode = $(this).data("zipcode");
-  console.log("del click event " + zipcode);
+    var zipcode = $(this).data("zipcode");
+    console.log("del click event " + zipcode);
 
-var baseURL = "https://data.cityofnewyork.us/resource/xx67-kt59.json?dba="
+    var baseURL = "https://data.cityofnewyork.us/resource/xx67-kt59.json?dba="
 
-var queryURL;
-
-      
-
-         queryURL = baseURL + restName + "&building="  + building  + "&zipcode=" + zipcode;
-
-         console.log(queryURL);
+    var queryURL;
 
 
-              $.ajax({
-                 url: queryURL,
-                 method: "GET"
-             })
-              // We store all of the retrieved data inside of an object called "response"
-             .done(function(response) {
+
+    queryURL = baseURL + restName + "&building=" + building + "&zipcode=" + zipcode;
+
+    console.log(queryURL);
 
 
-                  // For loop set max to 10 objects and index 10 assign attributed
-                 for (var i = 0; i < response.length; i++) {
-                     response[i]
-
-                     var restName = response[i].dba;
-                     console.log("Restaraunt Name: ", restName);
-
-                     var building = response[i].building;
-                     console.log("Address number: ", building);
-
-                     var street = response[i].street;
-                     console.log("Street Name: ", street)
-
-                     var zipcode = response[i].zipcode;
-                     console.log("Zipcode: ", zipcode);
-
-                      var boro = response[i].boro;
-                      console.log("City Boro: ", boro);
-
-                     var cuisineDescription = response[i].cuisine_description;
-                     console.log("Type of Food: ", cuisineDescription);
-
-                     var grade = response[i].grade;
-                     console.log("Grade: ", grade);
-
-                     var inspectionDate = response[i].inspection_date;
-                     console.log("Inpection_date: ", inspectionDate);
-
-                      var inspectionType = response[i].inspection_type;
-                      console.log("Inpection_type: ", inspectionType);
-
-                     var violationDescription = response[i].violation_description;
-                     console.log("Violation Description: ", violationDescription);
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // We store all of the retrieved data inside of an object called "response"
+        .done(function(response) {
 
 
-                     // for (key in violationDescription) {
+            // For loop set max to 10 objects and index 10 assign attributed
+            for (var i = 0; i < response.length; i++) {
+                response[i]
+
+                var restName = response[i].dba;
+                console.log("Restaraunt Name: ", restName);
+
+                var building = response[i].building;
+                console.log("Address number: ", building);
+
+                var street = response[i].street;
+                console.log("Street Name: ", street)
+
+                var zipcode = response[i].zipcode;
+                console.log("Zipcode: ", zipcode);
+
+                var boro = response[i].boro;
+                console.log("City Boro: ", boro);
+
+                var cuisineDescription = response[i].cuisine_description;
+                console.log("Type of Food: ", cuisineDescription);
+
+                var grade = response[i].grade;
+                console.log("Grade: ", grade);
+
+                var inspectionDate = response[i].inspection_date;
+                console.log("Inpection_date: ", inspectionDate);
+
+                var inspectionType = response[i].inspection_type;
+                console.log("Inpection_type: ", inspectionType);
+
+                var violationDescription = response[i].violation_description;
+                console.log("Violation Description: ", violationDescription);
 
 
-                     // }
-
-                     //  full list of items to the well and adding it appending to the DOM via JQuery
-                      $(".tableresponse tbody").append("<tr><td class='restaurant-name-display'> " + restName +
-                          "</td><td class='address-display-res'>" + (building + " " + street + zipcode + " " + boro) +
-                          "</td><td class='cusine-type-display-res'>" + cuisineDescription +
-                          "</td><td class='grade-display-res'>" + grade +
-                          "</td><td class='inspection-date-display-res'>" + inspectionDate +
-                           "</td><td class= 'inspection-type-display-res'>" +inspectionType +
-                          "</td><td class='violation-display-res'>" + violationDescription);
+                // Trying to get rid of undefined text
+                if (grade == undefined) {
+                    grade = "Information Unavailable";
+                }
+                // for (key in violationDescription) {
 
 
-     // alert($(this).attr("data-zipcode"));
-};
+                // }
+
+                //  full list of items to the well and adding it appending to the DOM via JQuery
+                $(".tableresponse tbody").append("<tr><td class='restaurant-name-display'> " + restName +
+                    "</td><td class='address-display-res'>" + (building + " " + street + zipcode + " " + boro) +
+                    "</td><td class='cusine-type-display-res'>" + cuisineDescription +
+                    "</td><td class='grade-display-res'>" + grade +
+                    "</td><td class='inspection-date-display-res'>" + inspectionDate +
+                    "</td><td class= 'inspection-type-display-res'>" + inspectionType +
+                    "</td><td class='violation-display-res'>" + violationDescription);
+
+
+                // alert($(this).attr("data-zipcode"));
+            };
+        });
+
 });
-});
+
